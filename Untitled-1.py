b9282c4c322fad499ec2ed363029616e576e92ee
TheSnowyPanda <paturidarshith@gmail.com>
2026-06-28 13:01:18 -0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,18 @@
+print("Hello, I am a huge Soccer/Football Fan, based in the US. ")
+print("Pick a team and type its acronym in")
+print("Algeria ALG")
+print("Argentina ARG")
+print("Australia AUS")
+print("Austria AUT")
+print("Belguim BEL")
+print("Bosnia and Heregovina BIH")
+print("Brazil BRA")
+print("Canada CAN")
+print("Cape Verde CPV")
+
+nation = input("Give me an acronym: ")
+
+if nation == "ALG":
+  playerType = input(print("Pick a Player position: Goal Keeper? - Type G, Forward? - Type F, Midfielders?-Type M, Defenders? - Type D: "))
+    if playerType == "G":
+        print("This is a W")
